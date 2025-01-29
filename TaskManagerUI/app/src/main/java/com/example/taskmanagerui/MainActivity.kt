e2550c456fd4d7dbe71416526f2818793787aefd
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2025-01-29 21:58:13 +0530

    created text

--- TaskManagerUI/app/src/main/java/com/example/taskmanagerui/MainActivity.kt
+++ TaskManagerUI/app/src/main/java/com/example/taskmanagerui/MainActivity.kt
@@ -4,11 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.taskmanagerui.ui.theme.TaskManagerUITheme
 
 class MainActivity : ComponentActivity() {
@@ -17,18 +24,34 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             TaskManagerUITheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-
-                }
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) { }
             }
         }
     }
 }
 
+@Composable
+fun TaskCompletedLayout(modifier: Modifier = Modifier) {
+    Column {
+        Text(
+            text = "All tasks completed",
+            fontWeight = FontWeight.Bold,
+            modifier = modifier.padding(top = 24.dp, bottom = 8.dp)
+        )
+        Text(
+            text = "Nice work!",
+            fontSize = 16.sp
+        )
+    }
+}
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     TaskManagerUITheme {
+        TaskCompletedLayout()
     }
 }
