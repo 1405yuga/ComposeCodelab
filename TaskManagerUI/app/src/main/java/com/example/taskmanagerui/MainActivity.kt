f60b33e1ce110e71a828e7feab73d7c87af56e11
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2025-01-29 22:08:07 +0530

    added image and column created around it

--- TaskManagerUI/app/src/main/java/com/example/taskmanagerui/MainActivity.kt
+++ TaskManagerUI/app/src/main/java/com/example/taskmanagerui/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -11,7 +13,10 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -27,7 +32,9 @@ class MainActivity : ComponentActivity() {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
-                ) { }
+                ) {
+                    TaskCompletedLayout(modifier = Modifier)
+                }
             }
         }
     }
@@ -35,7 +42,15 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun TaskCompletedLayout(modifier: Modifier = Modifier) {
-    Column {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(R.drawable.ic_task_completed),
+            contentDescription = null,
+            contentScale = ContentScale.Fit
+        )
         Text(
             text = "All tasks completed",
             fontWeight = FontWeight.Bold,
@@ -48,7 +63,7 @@ fun TaskCompletedLayout(modifier: Modifier = Modifier) {
     }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun GreetingPreview() {
     TaskManagerUITheme {
