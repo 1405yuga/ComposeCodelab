b2d677a065fe74791a42805f9710a18aabd8b7a7
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2025-01-30 10:58:33 +0530

    created bottom layout

--- BusinessCard/app/src/main/java/com/example/businesscard/MainActivity.kt
+++ BusinessCard/app/src/main/java/com/example/businesscard/MainActivity.kt
@@ -4,16 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Email
 import androidx.compose.material.icons.outlined.Phone
+import androidx.compose.material.icons.outlined.Share
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
@@ -32,10 +36,28 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
+fun BottomLayout() {
+    Column(
+        Modifier.padding(vertical = 4.dp, horizontal = 8.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        ContactInfoCard(
+            iconVector = Icons.Outlined.Phone, contactText = "+00 (00) 00000"
+        )
+        ContactInfoCard(
+            iconVector = Icons.Outlined.Share, contactText = "@AndroidDev"
+        )
+        ContactInfoCard(
+            iconVector = Icons.Outlined.Email, contactText = "joe.bind@android.com"
+        )
+    }
+}
+
+@Composable
 fun ContactInfoCard(iconVector: ImageVector, contactText: String, modifier: Modifier = Modifier) {
     Row(
         Modifier
-            .padding(8.dp)
+            .padding(vertical = 4.dp)
             .fillMaxWidth(),
         horizontalArrangement = Arrangement.Center
     ) {
@@ -54,8 +76,6 @@ fun ContactInfoCard(iconVector: ImageVector, contactText: String, modifier: Modi
 @Composable
 fun GreetingPreview() {
     BusinessCardTheme {
-        ContactInfoCard(
-            iconVector = Icons.Outlined.Phone, contactText = "+00 (00) 00000"
-        )
+        BottomLayout()
     }
 }
