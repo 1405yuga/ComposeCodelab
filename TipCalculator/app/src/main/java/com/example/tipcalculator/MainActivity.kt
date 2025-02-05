148f37a5af68f8f576f9119be3ccb7b7ca6b2486
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2025-02-05 17:50:00 +0530

    created mutablestate for amount

--- TipCalculator/app/src/main/java/com/example/tipcalculator/MainActivity.kt
+++ TipCalculator/app/src/main/java/com/example/tipcalculator/MainActivity.kt
@@ -17,6 +17,10 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -43,10 +47,10 @@ private fun calculateTip(amount: Double, tipPercent: Double = 15.0): String {
 
 @Composable
 fun EditNumberField(modifier: Modifier = Modifier) {
-    val amountInput = "0"
+    var amountInput by remember { mutableStateOf("") }
     TextField(
         value = amountInput,
-        onValueChange = {},
+        onValueChange = { amountInput = it },
         modifier = modifier
     )
 }
@@ -72,9 +76,11 @@ fun TipCalculatorLayout(modifier: Modifier = Modifier) {
                 .padding(bottom = 16.dp, top = 40.dp)
                 .align(alignment = Alignment.Start)
         )
-        EditNumberField(modifier = Modifier
-            .padding(bottom = 32.dp)
-            .fillMaxWidth())
+        EditNumberField(
+            modifier = Modifier
+                .padding(bottom = 32.dp)
+                .fillMaxWidth()
+        )
         Text(
             text = "Tip Amount: $ ${tipAmount}",
             style = MaterialTheme.typography.displaySmall
