e2cc1fc79eb23b93a13f50c7705c54b845d54787
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2025-02-17 22:34:02 +0530

    created ui basic structure

--- Affirmations/app/src/main/java/com/example/affirmations/MainActivity.kt
+++ Affirmations/app/src/main/java/com/example/affirmations/MainActivity.kt
@@ -4,12 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.affirmations.ui.theme.AffirmationsTheme
 
@@ -19,29 +14,19 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             AffirmationsTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                AffirmationsApp()
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun AffirmationsApp() {}
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    AffirmationsTheme {
-        Greeting("Android")
-    }
+@Preview(
+    showSystemUi = true,
+    showBackground = true
+)
+fun AffirmationsPreview() {
+    AffirmationsApp()
 }
