d9e80dfc3c2143dd9c44201ff2153884c35dd7f0
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2025-02-18 15:15:20 +0530

    attached top app bar

--- WoofList/app/src/main/java/com/example/wooflist/MainActivity.kt
+++ WoofList/app/src/main/java/com/example/wooflist/MainActivity.kt
@@ -16,10 +16,13 @@ import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
@@ -76,8 +79,7 @@ fun WoofItemCard(woof: Woof, modifier: Modifier = Modifier) {
                     modifier = Modifier.padding(top = dimensionResource(id = R.dimen.padding_small))
                 )
                 Text(
-                    text = "${woof.age} years old",
-                    style = MaterialTheme.typography.bodyLarge
+                    text = "${woof.age} years old", style = MaterialTheme.typography.bodyLarge
                 )
             }
         }
@@ -86,12 +88,13 @@ fun WoofItemCard(woof: Woof, modifier: Modifier = Modifier) {
 
 @Composable
 fun WoofListApp(modifier: Modifier = Modifier) {
-    Scaffold { it ->
+    Scaffold(
+        topBar = { WoofTopAppBar() }
+    ) { padding ->
         LazyColumn(
-            contentPadding = it,
+            contentPadding = padding,
             verticalArrangement = Arrangement.spacedBy(10.dp),
-            modifier = modifier
-                .statusBarsPadding()
+            modifier = modifier.statusBarsPadding()
         ) {
             items(WoofListData.dataList) { woof ->
                 WoofItemCard(woof = woof, modifier = modifier)
@@ -100,12 +103,38 @@ fun WoofListApp(modifier: Modifier = Modifier) {
     }
 }
 
+
 @Preview(showBackground = true)
 @Composable
 fun WoofListPreview() {
     WoofListTheme {
         WoofListApp(modifier = Modifier)
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun WoofTopAppBar(modifier: Modifier = Modifier) {
+    CenterAlignedTopAppBar(
+        modifier = modifier,
+        title = {
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Image(
+                    painter = painterResource(R.drawable.ic_woof_logo),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(dimensionResource(R.dimen.image_size))
+                        .padding(dimensionResource(R.dimen.padding_small))
+                )
+                Text(
+                    text = stringResource(R.string.app_name),
+                    style = MaterialTheme.typography.displayLarge
+                )
+            }
+        },
+    )
 }
 
 @Preview(showBackground = true)
