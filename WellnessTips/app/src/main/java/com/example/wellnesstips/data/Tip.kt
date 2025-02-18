7773ea2bc51b5cf9e5f4e24cd5879862d01131cc
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2025-02-19 00:46:42 +0530

    created data

---
+++ WellnessTips/app/src/main/java/com/example/wellnesstips/data/Tip.kt
@@ -0,0 +1,8 @@
+package com.example.wellnesstips.data
+
+import androidx.annotation.StringRes
+
+data class Tip(
+    @StringRes val title: Int,
+    @StringRes val description: Int
+)
---
+++ WellnessTips/app/src/main/java/com/example/wellnesstips/data/TipRepo.kt
@@ -0,0 +1,41 @@
+package com.example.wellnesstips.data
+
+import com.example.wellnesstips.R
+
+object TipRepo {
+    fun load30DayTips(): List<Tip> {
+        return listOf(
+            Tip(R.string.day1_title, R.string.day1_description),
+            Tip(R.string.day2_title, R.string.day2_description),
+            Tip(R.string.day3_title, R.string.day3_description),
+            Tip(R.string.day4_title, R.string.day4_description),
+            Tip(R.string.day5_title, R.string.day5_description),
+            Tip(R.string.day6_title, R.string.day6_description),
+            Tip(R.string.day7_title, R.string.day7_description),
+            Tip(R.string.day8_title, R.string.day8_description),
+            Tip(R.string.day9_title, R.string.day9_description),
+            Tip(R.string.day10_title, R.string.day10_description),
+            Tip(R.string.day11_title, R.string.day11_description),
+            Tip(R.string.day12_title, R.string.day12_description),
+            Tip(R.string.day13_title, R.string.day13_description),
+            Tip(R.string.day14_title, R.string.day14_description),
+            Tip(R.string.day15_title, R.string.day15_description),
+            Tip(R.string.day16_title, R.string.day16_description),
+            Tip(R.string.day17_title, R.string.day17_description),
+            Tip(R.string.day18_title, R.string.day18_description),
+            Tip(R.string.day19_title, R.string.day19_description),
+            Tip(R.string.day20_title, R.string.day20_description),
+            Tip(R.string.day21_title, R.string.day21_description),
+            Tip(R.string.day22_title, R.string.day22_description),
+            Tip(R.string.day23_title, R.string.day23_description),
+            Tip(R.string.day24_title, R.string.day24_description),
+            Tip(R.string.day25_title, R.string.day25_description),
+            Tip(R.string.day26_title, R.string.day26_description),
+            Tip(R.string.day27_title, R.string.day27_description),
+            Tip(R.string.day28_title, R.string.day28_description),
+            Tip(R.string.day29_title, R.string.day29_description),
+            Tip(R.string.day30_title, R.string.day30_description)
+
+        )
+    }
+}
