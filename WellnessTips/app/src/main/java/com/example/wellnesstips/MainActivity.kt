15df06ffd2eb44d5699006c8b30caf542f356b76
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2025-02-19 01:24:17 +0530

    added data to list

--- WellnessTips/app/src/main/java/com/example/wellnesstips/MainActivity.kt
+++ WellnessTips/app/src/main/java/com/example/wellnesstips/MainActivity.kt
@@ -4,8 +4,27 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.wellnesstips.data.Tip
+import com.example.wellnesstips.data.TipRepo
 import com.example.wellnesstips.ui.theme.WellnessTipsTheme
 
 class MainActivity : ComponentActivity() {
@@ -14,6 +33,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             WellnessTipsTheme {
+                WellnessTipsApp()
             }
         }
     }
@@ -28,6 +48,63 @@ fun WellnessPreview() {
 }
 
 @Composable
+fun TipItemCard(
+    tip: Tip,
+    index: Int,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        modifier = modifier,
+    ) {
+        Column(
+            verticalArrangement = Arrangement.spacedBy(8.dp),
+            modifier = Modifier.padding(12.dp)
+        ) {
+            Text(
+                text = "Day ${index + 1}",
+                style = MaterialTheme.typography.bodyLarge
+            )
+            Text(
+                text = stringResource(tip.title),
+                style = MaterialTheme.typography.displaySmall
+            )
+            Image(
+                painter = painterResource(tip.image),
+                contentDescription = null,
+                contentScale = ContentScale.FillBounds
+            )
+            Text(
+                text = stringResource(tip.description),
+                style = MaterialTheme.typography.displayMedium
+            )
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
 fun WellnessTipsApp() {
-    TODO("Not yet implemented")
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = "30 Days Of Wellness")
+                },
+                modifier = Modifier
+            )
+        }
+    ) { it ->
+        LazyColumn(
+            contentPadding = it,
+            modifier = Modifier
+                .padding(
+                    horizontal = 12.dp
+                ),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            itemsIndexed(TipRepo.load30DayTips()) { index, tip ->
+                TipItemCard(tip = tip, index = index)
+            }
+        }
+    }
 }
--- WellnessTips/app/src/main/java/com/example/wellnesstips/data/Tip.kt
+++ WellnessTips/app/src/main/java/com/example/wellnesstips/data/Tip.kt
@@ -1,8 +1,10 @@
 package com.example.wellnesstips.data
 
+import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 
 data class Tip(
     @StringRes val title: Int,
-    @StringRes val description: Int
+    @StringRes val description: Int,
+    @DrawableRes val image: Int
 )
