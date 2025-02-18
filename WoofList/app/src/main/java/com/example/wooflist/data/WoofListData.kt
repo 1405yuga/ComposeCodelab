a9f4d78bc03cc8d54197cae78810b603564d83ef
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2025-02-18 11:27:16 +0530

    added data and images in app

---
+++ WoofList/app/src/main/java/com/example/wooflist/data/WoofListData.kt
@@ -0,0 +1,17 @@
+package com.example.wooflist.data
+
+import com.example.wooflist.R
+
+object WoofListData {
+    val dataList = listOf(
+        Woof(R.drawable.koda, R.string.dog_name_1, 2, R.string.dog_description_1),
+        Woof(R.drawable.lola, R.string.dog_name_2, 16, R.string.dog_description_2),
+        Woof(R.drawable.frankie, R.string.dog_name_3, 2, R.string.dog_description_3),
+        Woof(R.drawable.nox, R.string.dog_name_4, 8, R.string.dog_description_4),
+        Woof(R.drawable.faye, R.string.dog_name_5, 8, R.string.dog_description_5),
+        Woof(R.drawable.bella, R.string.dog_name_6, 14, R.string.dog_description_6),
+        Woof(R.drawable.moana, R.string.dog_name_7, 2, R.string.dog_description_7),
+        Woof(R.drawable.tzeitel, R.string.dog_name_8, 7, R.string.dog_description_8),
+        Woof(R.drawable.leroy, R.string.dog_name_9, 4, R.string.dog_description_9)
+    )
+}
