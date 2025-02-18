b73b6dc9c6f4aade75b0a6a7cc9dd9738873684f
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2025-02-18 11:40:28 +0530

    created basic card

--- WoofList/app/src/main/java/com/example/wooflist/MainActivity.kt
+++ WoofList/app/src/main/java/com/example/wooflist/MainActivity.kt
@@ -4,10 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.material3.Card
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.wooflist.data.Woof
+import com.example.wooflist.data.WoofListData
 import com.example.wooflist.ui.theme.WoofListTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,7 +36,28 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun WoofItemCard(woof: Woof, modifier: Modifier = Modifier) {
-
+    Card(
+        modifier = modifier
+            .fillMaxWidth()
+            .wrapContentHeight()
+    ) {
+        Row {
+            Image(
+                painter = painterResource(woof.imageResourceId),
+                contentDescription = stringResource(woof.name),
+                contentScale = ContentScale.Inside,
+                modifier = modifier.weight(1f)
+            )
+            Column(
+                modifier = modifier
+                    .padding(horizontal = 8.dp, vertical = 4.dp)
+                    .weight(2f)
+            ) {
+                Text(text = stringResource(woof.name))
+                Text(text = "${woof.age} years old")
+            }
+        }
+    }
 }
 
 @Composable
@@ -33,4 +67,5 @@ fun WoofListApp(modifier: Modifier = Modifier) {
 @Preview(showBackground = true)
 @Composable
 fun WoofListPreview() {
+    WoofItemCard(WoofListData.dataList[0])
 }
