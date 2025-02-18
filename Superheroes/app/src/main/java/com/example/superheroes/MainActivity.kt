67b74cce3b25618579fa8315a08510e505faf961
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2025-02-19 00:09:12 +0530

    solved the issue of extra space after topbar and made image perfect square

--- Superheroes/app/src/main/java/com/example/superheroes/MainActivity.kt
+++ Superheroes/app/src/main/java/com/example/superheroes/MainActivity.kt
@@ -6,13 +6,16 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -25,6 +28,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
@@ -34,7 +38,6 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.superheroes.data.Hero
 import com.example.superheroes.data.HeroesRepository
-import com.example.superheroes.ui.theme.Shapes
 import com.example.superheroes.ui.theme.SuperheroesTheme
 
 class MainActivity : ComponentActivity() {
@@ -80,15 +83,20 @@ fun HeroItemCard(
                     style = MaterialTheme.typography.bodyLarge
                 )
             }
-            Image(
-                painter = painterResource(hero.imageRes),
-                contentDescription = null,
-                contentScale = ContentScale.FillBounds,
-                modifier = modifier
-                    .weight(1f)
-                    .clip(shape = Shapes.medium)
+            Spacer(Modifier.width(16.dp))
+            Box(
+                modifier = Modifier
                     .size(72.dp)
-            )
+                    .clip(RoundedCornerShape(8.dp))
+
+            ) {
+                Image(
+                    painter = painterResource(hero.imageRes),
+                    contentDescription = null,
+                    alignment = Alignment.TopCenter,
+                    contentScale = ContentScale.FillWidth
+                )
+            }
         }
     }
 
@@ -108,13 +116,12 @@ fun HeroApp(modifier: Modifier = Modifier) {
                 }
             )
         },
-        modifier = modifier
+        modifier = modifier.fillMaxSize()
     ) { it ->
         LazyColumn(
             contentPadding = it,
             verticalArrangement = Arrangement.spacedBy(8.dp),
             modifier = modifier
-                .statusBarsPadding()
                 .padding(horizontal = 16.dp)
         ) {
             items(HeroesRepository.heroes) { hero ->
