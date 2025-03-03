e937dc710c882908c101517cf6125a0a3a518bed
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2025-03-03 15:41:05 +0530

    tested - if first cake clicked ,second screen is opened

--- CupCake/app/src/androidTest/java/com/example/cupcake/CupCakeScreenNavigationTest.kt
+++ CupCake/app/src/androidTest/java/com/example/cupcake/CupCakeScreenNavigationTest.kt
@@ -4,6 +4,7 @@ import androidx.activity.ComponentActivity
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.compose.ui.test.performClick
 import androidx.navigation.compose.ComposeNavigator
 import androidx.navigation.testing.TestNavHostController
 import org.junit.Before
@@ -31,8 +32,14 @@ class CupCakeScreenNavigationTest {
     }
 
     @Test
-    fun cupcakeNavHost_verifyBackNavigationNotShownOnStartOrderScreen(){
+    fun cupcakeNavHost_verifyBackNavigationNotShownOnStartOrderScreen() {
         val backText = composeTestRule.activity.getString(R.string.back_button)
         composeTestRule.onNodeWithContentDescription(backText).assertDoesNotExist()
+    }
+
+    @Test
+    fun cupcakeNavHost_clickFirstCake_navigateToSelectFlavorScreen() {
+        composeTestRule.onNodeWithStringId(R.string.one_cupcake).performClick()
+        navController.assertCurrentRouteName(CupcakeScreen.Flavor.name)
     }
 }
