53feae736947ea1df0aae1d3e98a762f1a09f53f
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2025-03-03 18:37:28 +0530

    added controller and navHost and assigned routes

--- LunchTray/app/src/main/java/com/example/lunchtray/LunchTrayApp.kt
+++ LunchTray/app/src/main/java/com/example/lunchtray/LunchTrayApp.kt
@@ -1,28 +1,25 @@
-/*
- * Copyright (C) 2023 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.example.lunchtray
 
 import androidx.annotation.StringRes
-import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.lunchtray.datasource.DataSource
+import com.example.lunchtray.ui.AccompanimentMenuScreen
+import com.example.lunchtray.ui.CheckoutScreen
+import com.example.lunchtray.ui.EntreeMenuScreen
 import com.example.lunchtray.ui.OrderViewModel
+import com.example.lunchtray.ui.SideDishMenuScreen
+import com.example.lunchtray.ui.StartOrderScreen
 
 enum class LunchTrayScreen(@StringRes title: Int) {
     Start(R.string.app_name),
@@ -34,10 +31,18 @@ enum class LunchTrayScreen(@StringRes title: Int) {
 
 // TODO: AppBar
 
-@OptIn(ExperimentalMaterial3Api::class)
+private fun cancelOrderAndNavigateToStart(
+    viewModel: OrderViewModel,
+    navController: NavController
+) {
+    viewModel.resetOrder()
+    navController.popBackStack(LunchTrayScreen.Start.name, inclusive = false)
+}
+
 @Composable
 fun LunchTrayApp() {
     // TODO: Create Controller and initialization
+    val navController = rememberNavController()
 
     // Create ViewModel
     val viewModel: OrderViewModel = viewModel()
@@ -50,5 +55,84 @@ fun LunchTrayApp() {
         val uiState by viewModel.uiState.collectAsState()
 
         // TODO: Navigation host
+        NavHost(
+            navController = navController,
+            startDestination = LunchTrayScreen.Start.name,
+            modifier = Modifier.padding(innerPadding)
+        ) {
+            composable(route = LunchTrayScreen.Start.name) {
+                StartOrderScreen(
+                    onStartOrderButtonClicked = { navController.navigate(LunchTrayScreen.EntreeMenu.name) },
+                    modifier = Modifier.fillMaxSize()
+                )
+            }
+
+            composable(route = LunchTrayScreen.EntreeMenu.name) {
+                EntreeMenuScreen(
+                    options = DataSource.entreeMenuItems,
+                    onCancelButtonClicked = {
+                        cancelOrderAndNavigateToStart(
+                            viewModel,
+                            navController
+                        )
+                    },
+                    onNextButtonClicked = {
+                        navController.navigate(LunchTrayScreen.SideDishMenu.name)
+                    },
+                    onSelectionChanged = { viewModel.updateEntree(it) },
+                    modifier = Modifier.fillMaxSize(),
+                )
+            }
+
+            composable(route = LunchTrayScreen.SideDishMenu.name) {
+                SideDishMenuScreen(
+                    options = DataSource.sideDishMenuItems,
+                    onCancelButtonClicked = {
+                        cancelOrderAndNavigateToStart(
+                            viewModel,
+                            navController
+                        )
+                    },
+                    onNextButtonClicked = { navController.navigate(LunchTrayScreen.AccompanimentMenu.name) },
+                    onSelectionChanged = { viewModel.updateSideDish(it) },
+                    modifier = Modifier.fillMaxSize()
+                )
+            }
+
+            composable(route = LunchTrayScreen.AccompanimentMenu.name) {
+                AccompanimentMenuScreen(
+                    options = DataSource.accompanimentMenuItems,
+                    onCancelButtonClicked = {
+                        cancelOrderAndNavigateToStart(
+                            viewModel,
+                            navController
+                        )
+                    },
+                    onNextButtonClicked = { navController.navigate(LunchTrayScreen.Checkout.name) },
+                    onSelectionChanged = { viewModel.updateAccompaniment(it) },
+                    modifier = Modifier.fillMaxSize()
+                )
+            }
+
+            composable(route = LunchTrayScreen.Checkout.name) {
+                CheckoutScreen(
+                    modifier = Modifier.fillMaxSize(),
+                    orderUiState = uiState,
+                    onNextButtonClicked = {
+                        cancelOrderAndNavigateToStart(
+                            viewModel, navController
+                        )
+                    },
+                    onCancelButtonClicked = {
+                        cancelOrderAndNavigateToStart(
+                            viewModel,
+                            navController
+                        )
+                    }
+                )
+            }
+
+        }
     }
 }
+
