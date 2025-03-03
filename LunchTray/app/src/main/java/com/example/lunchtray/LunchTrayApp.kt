c8e27c55ebe1fc1bfb9c4de96a4428a65a2166b3
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2025-03-03 16:18:07 +0530

    created enum for screens1

---
+++ LunchTray/app/src/main/java/com/example/lunchtray/LunchTrayApp.kt
@@ -0,0 +1,15 @@
+package com.example.lunchtray
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun LunchTrayApp(){
+
+}
+
+@Preview
+@Composable
+fun LunchTrayScreenPreview(){
+
+}
--- LunchTray/app/src/main/java/com/example/lunchtray/MainActivity.kt
+++ LunchTray/app/src/main/java/com/example/lunchtray/MainActivity.kt
@@ -4,13 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.annotation.StringRes
 import com.example.lunchtray.ui.theme.LunchTrayTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +13,16 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             LunchTrayTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                LunchTrayApp()
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    LunchTrayTheme {
-        Greeting("Android")
-    }
+enum class LunchTrayScreen(@StringRes title: Int) {
+    Start(R.string.app_name),
+    EntreeMenu(R.string.choose_entree),
+    SideDishMenu(R.string.choose_side_dish),
+    AccompanimentMenu(R.string.choose_accompaniment),
+    Checkout(R.string.order_checkout)
 }
