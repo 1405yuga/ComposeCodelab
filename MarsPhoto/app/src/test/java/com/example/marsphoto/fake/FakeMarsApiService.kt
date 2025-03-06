d12b4d79859f867af30046c45a91b9815b16a7c1
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2025-03-06 15:54:33 +0530

    created fake data and dependencies for test

---
+++ MarsPhoto/app/src/test/java/com/example/marsphoto/fake/FakeMarsApiService.kt
@@ -0,0 +1,10 @@
+package com.example.marsphoto.fake
+
+import com.example.marsphoto.network.MarsApiService
+import com.example.marsphoto.network.MarsPhoto
+
+class FakeMarsApiService : MarsApiService {
+    override suspend fun getPhotos(): List<MarsPhoto> {
+        return FakeDataSource.photosList
+    }
+}
