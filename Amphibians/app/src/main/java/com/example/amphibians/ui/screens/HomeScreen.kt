c77630e9d1c06286e28583d66c8cce3c6087c7aa
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2025-03-07 16:42:48 +0530

    displayed list with image

--- Amphibians/app/src/main/java/com/example/amphibians/ui/screens/HomeScreen.kt
+++ Amphibians/app/src/main/java/com/example/amphibians/ui/screens/HomeScreen.kt
@@ -1,13 +1,30 @@
 package com.example.amphibians.ui.screens
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+import com.example.amphibians.R
+import com.example.amphibians.model.Amphibian
 
 @Composable
 fun HomeScreen(
@@ -21,10 +38,71 @@ fun HomeScreen(
             .padding(contentPadding)
     ) {
         when (amphibianUiState) {
-            is AmphibianUiState.Success -> Text("Hello")
+            is AmphibianUiState.Success -> AmphibianList(amphibianUiState.amphibianList)
             is AmphibianUiState.Error -> ErrorScreen()
             is AmphibianUiState.Loading -> LoadingScreen()
         }
     }
 
+}
+
+@Composable
+fun AmphibianList(
+    amphibians: List<Amphibian>,
+    modifier: Modifier = Modifier
+) {
+    LazyColumn(
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+        modifier = modifier.padding(horizontal = 12.dp)
+    ) {
+        items(amphibians) { amphibian ->
+            AmphibianCard(amphibian = amphibian)
+        }
+    }
+}
+
+@Composable
+fun AmphibianCard(amphibian: Amphibian, modifier: Modifier = Modifier) {
+    Card(
+        modifier = modifier,
+        elevation = CardDefaults.cardElevation(defaultElevation = 8.dp),
+        shape = RoundedCornerShape(8.dp)
+    ) {
+        Column {
+            Text(
+                text = "${amphibian.name} (${amphibian.type})",
+                fontWeight = FontWeight.ExtraBold,
+                fontSize = 22.sp,
+                modifier = Modifier.padding(12.dp)
+            )
+            AsyncImage(
+                model = ImageRequest.Builder(context = LocalContext.current)
+                    .data(amphibian.imgSrc)
+                    .crossfade(true)
+                    .build(),
+                error = painterResource(R.drawable.ic_broken_image),
+                placeholder = painterResource(R.drawable.loading_img),
+                contentDescription = null,
+                modifier = Modifier.fillMaxWidth()
+            )
+            Text(
+                text = amphibian.description,
+                modifier = Modifier.padding(12.dp),
+                fontSize = 18.sp
+            )
+        }
+    }
+}
+
+@Composable
+@Preview
+fun AmphibianPreview() {
+    val mockData = List(10) { Amphibian("Frog", "Some type", "descrption", "com.a") }
+    AmphibianList(mockData)
+}
+
+@Composable
+@Preview
+fun AmphibianCardPreview() {
+    AmphibianCard(Amphibian("Frog", "Some type", "descrption", "com.a"))
 }
