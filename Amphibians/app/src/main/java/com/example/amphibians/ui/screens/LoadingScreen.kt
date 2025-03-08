cff49746f567af01e530bd1a4f12f4d18d5a2538
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2025-03-08 11:58:04 +0530

    added loading ui

--- Amphibians/app/src/main/java/com/example/amphibians/ui/screens/LoadingScreen.kt
+++ Amphibians/app/src/main/java/com/example/amphibians/ui/screens/LoadingScreen.kt
@@ -1,9 +1,24 @@
 package com.example.amphibians.ui.screens
 
-import androidx.compose.material3.Text
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.amphibians.R
 
 @Composable
-fun LoadingScreen() {
-    Text("Loading..")
+fun LoadingScreen(modifier: Modifier = Modifier) {
+    Image(
+        painter = painterResource(R.drawable.loading_img),
+        contentDescription = "Loading data",
+        modifier = modifier.fillMaxSize()
+    )
+}
+
+@Composable
+@Preview
+fun LoadingPreview() {
+    LoadingScreen()
 }
