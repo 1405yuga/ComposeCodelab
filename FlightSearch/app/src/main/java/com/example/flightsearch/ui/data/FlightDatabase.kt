a183ca62e828bea7b67ce9e3295977eb12e2f889
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2025-03-13 11:34:23 +0530

    fixed the issue : clearing data ondestroy

--- FlightSearch/app/src/main/java/com/example/flightsearch/ui/data/FlightDatabase.kt
+++ FlightSearch/app/src/main/java/com/example/flightsearch/ui/data/FlightDatabase.kt
@@ -5,7 +5,7 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = [AirportDetails::class, Favorite::class], version = 1, exportSchema = false)
+@Database(entities = [AirportDetails::class, Favorite::class], version = 2, exportSchema = false)
 abstract class FlightDatabase : RoomDatabase() {
     abstract fun flightDao(): FlightDao
 
@@ -15,7 +15,7 @@ abstract class FlightDatabase : RoomDatabase() {
         fun getDatabase(context: Context): FlightDatabase {
             return INSTANCE ?: synchronized(this) {
                 Room.databaseBuilder(context, FlightDatabase::class.java, "airport_database")
-                    .fallbackToDestructiveMigration()
+//                    .fallbackToDestructiveMigration()
                     .createFromAsset("flight_search.db")
                     .build()
                     .also { INSTANCE = it }
