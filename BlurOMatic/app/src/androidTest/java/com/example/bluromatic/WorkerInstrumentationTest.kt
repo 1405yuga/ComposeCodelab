990c3755354432e8d3844292ff848ea7bc32e443
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2025-03-18 12:22:53 +0530

    created test for blurWorker

--- BlurOMatic/app/src/androidTest/java/com/example/bluromatic/WorkerInstrumentationTest.kt
+++ BlurOMatic/app/src/androidTest/java/com/example/bluromatic/WorkerInstrumentationTest.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import androidx.test.core.app.ApplicationProvider
 import androidx.work.ListenableWorker
 import androidx.work.testing.TestListenableWorkerBuilder
+import androidx.work.workDataOf
+import com.example.bluromatic.workers.BlurWorker
 import com.example.bluromatic.workers.CleanupWorker
 import kotlinx.coroutines.runBlocking
 import org.junit.Assert.assertTrue
@@ -13,18 +15,37 @@ import org.junit.Test
 class WorkerInstrumentationTest {
 
     private lateinit var context: Context
+    val mockImageUri =
+        KEY_IMAGE_URI to "android.resource://com.example.bluromatic/drawable/android_cupcake"
 
     @Before
-    fun setUp(){
+    fun setUp() {
         context = ApplicationProvider.getApplicationContext()
     }
 
     @Test
-    fun cleanupWorker_doWork_resultSuccess(){
+    fun cleanupWorker_doWork_resultSuccess() {
         val worker = TestListenableWorkerBuilder<CleanupWorker>(context).build()
         runBlocking {
             val result = worker.doWork()
             assertTrue(result is ListenableWorker.Result.Success)
+        }
+    }
+
+    @Test
+    fun blurWorker_doWork_resultSuccessReturnUri() {
+        val worker = TestListenableWorkerBuilder<BlurWorker>(context)
+            .setInputData(workDataOf(mockImageUri))
+            .build()
+        runBlocking {
+            val result = worker.doWork()
+            val resultUri = result.outputData.getString(KEY_IMAGE_URI)
+            assertTrue(result is ListenableWorker.Result.Success)
+            assertTrue(result.outputData.keyValueMap.containsKey(KEY_IMAGE_URI))
+            assertTrue(
+                resultUri?.startsWith("file:///data/user/0/com.example.bluromatic/files/blur_filter_outputs/blur-filter-output-")
+                    ?: false
+            )
         }
     }
 }
